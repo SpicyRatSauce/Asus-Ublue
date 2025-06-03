b900235e80d98b7e78b13de9d9b10a52f4b4254d
RatSauce <58752704+SpicyRatSauce@users.noreply.github.com>
2025-06-03 20:36:57 -0300

    Update build.sh
    Added remove:
    - steam
    - fw-fanctrl

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,8 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+dnf5 remove -y steam fw-fanctrl
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
